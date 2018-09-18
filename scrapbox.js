db1565447994b3b705fb736ab78b88e5c4824b1e
TANAKA Kotaro <kotaro@zerobase.jp>
2018-09-18 14:54:01 +0900

    scrapbox bookmarklet

---
+++ scrapbox.js
@@ -0,0 +1,13 @@
+function execBookmarklet() {
+  var title = window.prompt('Register to dotimpact', document.title);
+  if (!title) return;
+  var lines = ['', document.title + '[~ ' + window.location.href + ']'];
+  var quote = window.getSelection().toString();
+  if (quote.trim()) lines = lines.concat(quote.split(/\n/g).map(function (line) {
+    return ' > ' + line;
+  }));
+  var d = new Date();
+  lines.push('#' + d.getFullYear() + '-' + (('0' + (d.getMonth() + 1)).slice(-2)) + '-' + (('0' + d.getDate()).slice(-2)));
+  var body = encodeURIComponent(lines.join('\n'));
+  window.open('https://scrapbox.io/dotimpact/' + encodeURIComponent(title.trim()) + '?body=' + body);
+}
