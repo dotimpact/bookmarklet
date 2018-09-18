0b821dd24f497a4dba9bdf2b936b3f6da76c9921
TANAKA Kotaro <kotaro@zerobase.jp>
2018-09-18 15:09:31 +0900

    template

---
+++ bookmarklet-template.js
@@ -0,0 +1,1 @@
+javascript:(function(){ var s = document.createElement("script"); s.src = "//rawgit.com/[github path]"; document.body.appendChild(s); setTimeout(function() { execBookmarklet();}, 100); })()
